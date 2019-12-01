c635802472ace3d7d8cccd53c5b4904a3d83f569
berryheerschop <berryheerschop@gmail.com>
2019-12-01 12:58:42 +0100

    developing script

--- hospitalquality.R
+++ hospitalquality.R
@@ -1,6 +1,20 @@
 best <- function(state, outcome) { 
     
     ## Read outcome data
+    outcome_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    
+    # Select column names
+    column_names <- names(outcome_data)
+    
+    # Relevant string from column names
+    relevant_colstring <- "Hospital.30.Day.Death..Mortality..Rates.from."
+    given_outcome_measure <- tolower(paste(relevant_colstring,outcome,sep='.'))
+    
+    # Determine unique states
+    unique_states <- unique(outcome_data$state)
+    
     ## Check that state and outcome are valid
+    state_valid <- is.element(state,unique_states)
+    
     ## Return hospital name in that state with lowest 30-day death ## rate
 }
